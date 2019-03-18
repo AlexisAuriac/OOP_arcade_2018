5f5119c27436b485f620bcfebe62d5c0d17f1374
ZANKLAN <Jesuyon@localhost.localdomain>
2019-03-18 22:58:34 +0100

    check error

--- include/Arcade.hpp
+++ include/Arcade.hpp
@@ -8,9 +8,35 @@
 #ifndef ARCADE_HPP
 #define ARCADE_HPP
 
+#include <iostream>
+#include <exception>
+#include <elf.h>
+
 namespace arc {
     static const int SUCCESS = 0;
     static const int FAILURE = 84;
-}
 
+    class Arcade {
+    public:
+        Arcade()
+        ~Arcade();
+
+        int run(int ac, char **av);
+    };
+
+    class Error : public std::exception {
+    public:
+        Error(std::string const& msg = "")
+        : _msg(msg)
+        {}
+        
+        const char *what() const no_except override
+        {
+            return _msg.c_str();
+        }
+
+    private:
+        const std::string _msg;
+    };
+}
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,42 @@
 #include <iostream>
 #include "Arcade.hpp"
 
+int error_handling(int ac , char **av)
+{
+	Elf64_Ehdr *elf;
+	void *data;
+	FILE *file  = fopen(av[1], "rb");
+
+	if (ac > 2)
+		throw Error("too many argument");
+	else if (file) {
+		fread(&elf, 1, sizeof(elf), file);
+		if (elf->e_ident[EI_MAG0] != 0x7f
+			|| elf->e_ident[EI_MAG1] != 'E'
+			|| elf->e_ident[EI_MAG2] != 'L'
+			|| elf->e_ident[EI_MAG3] != 'F')
+				throw Error("is not an ELF file");
+		fclose(file);
+	} else {
+		std::cout << "ok" << std::endl
+	}
+	
+}
+
+int Arcade::run(int ac , char **av)
+{
+	try {
+		error_handling(ac, av)
+	} catch(const arc::Error& e) {
+		std::cerr << "Error : "  << e.what() << std::endl;
+		return arc::FAILURE;
+	}
+	return (0)
+}
+
 int main(int ac, char **av)
 {
-	return arc::SUCCESS;
+	Arcade app;
+
+	return app.run(ac, av);	
 }

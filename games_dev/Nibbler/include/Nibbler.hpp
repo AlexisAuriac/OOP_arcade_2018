1dfc54101202a248a688c8af3af1c5ad53fa03b7
alexis <alexis.auriac@epitech.eu>
2019-04-01 17:35:34 +0200

    coding style improvements

--- games_dev/Nibbler/include/Nibbler.hpp
+++ games_dev/Nibbler/include/Nibbler.hpp
@@ -16,35 +16,35 @@
 
 namespace arc::game {
     class Nibbler : public IGame {
-        public:
-            void init(gl::IGraphicLib *gl) override;
-            std::pair<state, int> play(gl::event_t event) override;
-            void display() override;
-            void restart() override;
-            void changeGl(gl::IGraphicLib *gl) override;
+    public:
+        void init(gl::IGraphicLib *gl) override;
+        std::pair<state, int> play(gl::event_t event) override;
+        void display() override;
+        void restart() override;
+        void changeGl(gl::IGraphicLib *gl) override;
 
-        private:
-            void posFruit(int flag);
-            void manageHit();
-            void moveSnake();
-            void dirSnake(gl::event_t event);
-            void manageEvent(gl::event_t event);
+    private:
+        void posFruit(int flag);
+        void manageHit();
+        void moveSnake();
+        void dirSnake(gl::event_t event);
+        void manageEvent(gl::event_t event);
 
-            void drawMap();
-            void printScore();
-            void printSnake();
+        void drawMap();
+        void printScore();
+        void printSnake();
 
-        private:
-            gl::IGraphicLib *_gl;
-            int _row;
-            int _col;
-            int _run;
-            int _index;
-            int _movx;
-            int _movy;
-            std::pair<int, int> _fruit;
-            std::vector<std::pair<int, int>> _tail;
-            std::vector<std::pair<int, useconds_t>> _tabScore;
+    private:
+        gl::IGraphicLib *_gl;
+        int _row;
+        int _col;
+        int _run;
+        int _index;
+        int _movx;
+        int _movy;
+        std::pair<int, int> _fruit;
+        std::vector<std::pair<int, int>> _tail;
+        std::vector<std::pair<int, useconds_t>> _tabScore;
     };
 }
 
